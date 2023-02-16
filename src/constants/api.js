0fe47c04e2818ca3da57742d046cd3dda5334578
unknown <atmankakadiya007@gmail.com>
2023-02-16 16:57:50 +0530

    add orignal codfe

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,7 +1,7 @@
 // export const API_URL_OLD = 'https://searchnsettle.trigma.in/web/v1/'
 // export const API_URL = 'https://searchnsettle.com.au:3002/api/'
 
-export const API_URL = 'http://3.26.194.73:3002/'
+export const API_URL = 'http://3.26.101.66:3002/api/'
 
 // export const API_URL_OLD = 'https://development.trigma.in:3002/web/v1/'
 // export const API_URL = 'http://localhost:3002/api/'
